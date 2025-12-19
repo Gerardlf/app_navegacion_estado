f4efa831cb8a82cfb781505220fa38e464a8a3ac
Gerardo de la fuente <gerdeluri@alu.edu.gva.es>
2025-12-19 20:08:09 +0100

    Test

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -27,81 +27,86 @@ class DetailProduct extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 12),
-              Text(" ${producto.id}", style: TextStyle(fontSize: 12)),
-              SizedBox(height: 12),
-              Icon(producto.iconData, size: 100, color: Colors.indigo),
-              Text(
-                " ${producto.nombre}",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 12),
-              Text(
-                " ${producto.precio} €",
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.indigo,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 12),
+                Text(" ${producto.id}", style: TextStyle(fontSize: 12)),
+                SizedBox(height: 12),
+                Icon(producto.iconData, size: 100, color: Colors.indigo),
+                Text(
+                  " ${producto.nombre}",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(height: 30),
-              Text(
-                "Descripción:",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-              ),
-              SizedBox(height: 20),
-              Text(
-                producto.descripcion,
-                style: TextStyle(
-                  fontSize: 13,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.grey[800],
+                SizedBox(height: 12),
+                Text(
+                  " ${producto.precio} €",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.indigo,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blueAccent,
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    onPressed: () => cart.addProduct(producto),
-                    icon: Icon(Icons.add_shopping_cart, color: Colors.white),
-                    label: Text(
-                      "Añadir al carrito",
-                      style: TextStyle(fontSize: 16, color: Colors.white),
+                SizedBox(height: 30),
+                Text(
+                  "Descripción:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  producto.descripcion,
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey[800],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueAccent,
+                        minimumSize: const Size.fromHeight(50),
+                      ),
+                      onPressed: () => cart.addProduct(producto),
+                      icon: Icon(Icons.add_shopping_cart, color: Colors.white),
+                      label: Text(
+                        "Añadir al carrito",
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    style: ElevatedButton.styleFrom(
-                      foregroundColor: Colors.blueAccent,
-                      minimumSize: const Size.fromHeight(50),
-                    ),
-                    onPressed: () => context.goNamed("cart"),
-                    icon: Icon(
-                      Icons.shopping_basket_outlined,
-                      color: Colors.blueAccent,
-                    ),
-                    label: Text(
-                      "ir al carrito",
-                      style: TextStyle(fontSize: 16, color: Colors.blueAccent),
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.blueAccent,
+                        minimumSize: const Size.fromHeight(50),
+                      ),
+                      onPressed: () => context.goNamed("cart"),
+                      icon: Icon(
+                        Icons.shopping_basket_outlined,
+                        color: Colors.blueAccent,
+                      ),
+                      label: Text(
+                        "Ir al carrito",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.blueAccent,
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
